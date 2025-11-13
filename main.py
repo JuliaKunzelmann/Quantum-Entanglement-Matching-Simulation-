5f95a788b03a865687ad147b680a65112fd54bc1
Luis Gindorf <luis.gindorf@student.uni-tuebingen.de>
2025-11-13 10:16:27 +0100

    Added if name == main condition

--- main.py
+++ main.py
@@ -8,30 +8,34 @@ from tqdm import tqdm
 random.seed(5142412)
 np.random.seed(412412)
 
-l_S1 = [] 
-l_S2 = []
-l_exact = []
+def main():
+	l_S1 = [] 
+	l_S2 = []
+	l_exact = []
 
-# Prepare histogram
-c1 = [0]*(m+1)
-c2 = [0]*(m+1)
+	# Prepare histogram
+	c1 = [0]*(m+1)
+	c2 = [0]*(m+1)
 
-for i in tqdm(range(repetitions)):
-	l_1, l_2, l_opt = Simulate_single_repetition()
-	l_S1.append(l_1)
-	l_S2.append(l_2)
-	l_exact.append(l_opt)
+	for i in tqdm(range(repetitions)):
+		l_1, l_2, l_opt = Simulate_single_repetition()
+		l_S1.append(l_1)
+		l_S2.append(l_2)
+		l_exact.append(l_opt)
 
-# Get average cardinality over all repetitions of exact matching 
-print("Average cardinality (exact algorithm): ", sum(l_exact)/repetitions)
+	# Get average cardinality over all repetitions of exact matching 
+	print("Average cardinality (exact algorithm): ", sum(l_exact)/repetitions)
 
-for r in range(repetitions):
-	assert l_S1[r] <= l_exact[r], "S1 found a larger matching than the exact. This hints at an error in the code"
-	assert l_S2[r] <= l_exact[r], "S2 found a larger matching than the exact. This hints at an error in the code"
+	for r in range(repetitions):
+		assert l_S1[r] <= l_exact[r], "S1 found a larger matching than the exact. This hints at an error in the code"
+		assert l_S2[r] <= l_exact[r], "S2 found a larger matching than the exact. This hints at an error in the code"
 
-	c1[l_exact[r] - l_S1[r]] += 1
-	c2[l_exact[r] - l_S2[r]] += 1
+		c1[l_exact[r] - l_S1[r]] += 1
+		c2[l_exact[r] - l_S2[r]] += 1
 
-print("Histograms of difference between exact and heuristic matching cardinality:")
-print("Strategy 1", c1)
-print("Strategy 2", c2)
+	print("Histograms of difference between exact and heuristic matching cardinality:")
+	print("Strategy 1", c1)
+	print("Strategy 2", c2)
+
+if __name__ == "__main__":
+	main()
